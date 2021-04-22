d18e915eb53b5ecf4a5e2a579207db4667247656
Keith Murphy <keith@flickerbox.com>
2021-04-21 23:35:54 -0700

    Stub out SCSS files and base components

---
+++ components/Preview.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>This is a preview</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Preview',
+}
+</script>
---
+++ components/Types.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Types</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Types',
+}
+</script>
+
+<style scoped>
+
+</style>
